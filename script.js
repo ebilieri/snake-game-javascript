9009b85de4b582a51db8a71f7e5413e1954e59a8
Emerson Claudelino <tc.claudelino@kantaribopemedia.com>
2020-05-27 12:57:03 -0300

    Criando o canvas

---
+++ script.js
@@ -0,0 +1,18 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = {
+
+}
+
+function criarBG(){
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha(){
+
+}
+
+criarBG();
